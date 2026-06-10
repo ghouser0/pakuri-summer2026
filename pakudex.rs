c73db333cacb4b532f3c8596ba1561d62099f8ba
gabs <gabrielahouser1@gmail.com>
2026-06-10 22:42:21 +0900

    worked a bit on stuff

---
+++ pakudex.rs
@@ -0,0 +1,50 @@
+use std::io::{Write, stdout, stdin};
+use std::collections::HashMap;
+
+fn display_menu()
+{   
+    println!("hiii");
+}
+
+fn list_pakuri()
+{   
+    // number and list pakuri
+}
+
+fn show_pakuri(String: name) -> bool
+{   
+    // returns false if pakuri doesn't exist
+}
+
+fn add_pakuri(String: name) -> bool
+{   
+    // returns false if add successful
+}
+
+fn kill_pakuri(String: name) -> bool
+{   
+    // returns false if remove successful
+}
+
+fn change_pakuri_level(String: name, Int32: level) -> bool
+{   
+    // returns false if add successful
+    // enter name
+    // enter level
+
+}
+
+fn main() 
+{
+    let mut menu_choice = String::new(); 
+    let mut menu_choice_casted: i32 = 0;   
+    
+    let mut previous_result = 0.0;
+    let mut sum_calculations = 0.0;
+    let mut total_calculations: i32 = 0;
+    
+    let mut print_da_menu: bool = true;
+
+    loop {}
+
+}
--- pakuri.rs
+++ pakuri.rs
@@ -1,1 +1,47 @@
 // pakuri struct
+
+
+
+struct Pakuri {
+    let mut name: String;
+    let mut species: String;
+    let mut level: i32;
+    let mut hp: i32;
+    let mut cp: i32;
+
+
+    pub new(name: String, species: String) -> Self {
+        // idk girl !
+    }
+
+    pub with(name: String, species: String, level: i32) -> Self {
+        // idk what to return here
+    }
+
+    pub name(&self) -> &String {
+        return name;
+    }
+
+    pub species(&self) -> &String {
+        return species;
+    }
+
+    pub hp(&self) -> i32 {
+        // calculate hp
+        return hp;
+    }
+
+    pub cp(&self) -> i32 {
+        // calculate cp
+        return cp;
+    }
+
+    pub level(&self) -> i32 {
+        return level;
+    }
+
+    pub set_level(&self) -> i32 {
+        // set level field
+        return level;
+    }
+}
